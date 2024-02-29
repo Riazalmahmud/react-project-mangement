64f0e6fc9344b446aa15db891b1afa69def39038
riazalmahmud041@gmail.com <riazalmahmud041@gmail.com>
2024-02-29 15:05:18 +0600

    task bord

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 
 
 import './App.css'
-import Board from './pages/Board/Board';
-import { generateQuoteMap } from './pages/Dnd/mock-data';
+import Board from './pages/Board/Board'
+
+
 
 
 function App() {
 
-  const data = {
-    medium: generateQuoteMap(100),
-    large: generateQuoteMap(500)
-  };
+
   return (
     <>
 <h1>dashboard Task Management </h1>
-<Board initial={data.medium} withScrollableColumns />
+<Board/>
     </>
   )
 }
